e04a23cf2c6505914359e11abebc7a0987e04229
unknown <murtaza.bhai13@gmail.com>
2018-10-20 02:39:14 +0500

    Almost Done

---
+++ Assets/3d/F_fish/Fish.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Fish : MonoBehaviour
+{
+    float originalY;
+
+    public float floatStrength = 8; // You can change this in the Unity Editor to 
+                                    // change the range of y positions that are possible.
+
+    void Start()
+    {
+        this.originalY = this.transform.position.x;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.y,
+            originalY + ((float)Math.Sin(Time.time) * floatStrength),
+            transform.position.x);
+    }
+}
---
+++ Assets/3d/Q_question/Questionrotate.cs
@@ -0,0 +1,30 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+// Makes objects float up & down while gently spinning.
+public class Questionrotate : MonoBehaviour
+{
+public void Start()
+{
+StartCoroutine("Rotate90");
+}
+
+public IEnumerator Rotate90()
+{
+Vector3 axis = Vector3.up;
+float angle = 90.0f;
+float rotAmount = 0.0f;
+
+Quaternion startingRot = transform.rotation;
+
+while (rotAmount < angle)
+{
+rotAmount = Mathf.SmoothStep(0.0f, angle, Time.deltaTime);
+transform.RotateAround(axis, rotAmount);
+yield return 0;
+}
+}
+
+}
