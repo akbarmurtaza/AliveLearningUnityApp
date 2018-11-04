26a7908185f943c67f9a50afa0d105075300c945
unknown <murtaza.bhai13@gmail.com>
2018-11-04 15:04:07 +0500

    UI added and functional

---
+++ Assets/Scenes/buttonscode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonscode : MonoBehaviour {
+
+	public void startbutton (string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void more (string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void backbtnmore(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void backbtnsamplescene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+}
